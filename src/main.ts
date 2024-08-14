cd29ec7c03e3e24c3953d9332f640bb2cd72d6fc
AtharvaKamble <atharvadkamble00@gmail.com>
2024-08-14 19:44:17 -0400

    basic styling and adjust scene lighting

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import "./style.css";
 import * as THREE from "three";
 import { MapControls } from "three/addons/controls/MapControls.js";
 import * as BufferGeometryUtils from "three/addons/utils/BufferGeometryUtils.js";
+import Stats from "stats.js";
 
 import NOISE from "./perlin";
 
@@ -11,20 +12,23 @@ function getImageUrl(name: string) {
   //return new URL(`/textures/block/${name}.png`, import.meta.url).href;
 }
 
-const { log } = console;
+// const { log } = console;
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
-  60,
+  800,
   window.innerWidth / window.innerHeight,
-  0.1,
+  10,
   500,
 );
 
-scene.background = new THREE.Color(0xa6a4a4);
+//scene.background = new THREE.Color(0x01144a);
+scene.background = new THREE.Color(0x08001f);
 scene.fog = new THREE.FogExp2(0xcccccc, 0.002);
 
-const renderer = new THREE.WebGLRenderer({ antialias: true });
+const renderer = new THREE.WebGLRenderer({
+  antialias: true,
+});
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -57,11 +61,11 @@ const n = new NOISE(Math.random());
 
 const group = new THREE.Group();
 
-camera.position.x = 50;
-camera.position.y = 15;
-camera.position.z = 50;
+camera.position.x = 200;
+camera.position.y = 30;
+camera.position.z = 180;
 
-camera.rotateX(10);
+camera.rotation.z = 0.6;
 
 const NOON = 0xeeaf61;
 // const DUSK = 0xfb9062;
@@ -69,7 +73,7 @@ const NOON = 0xeeaf61;
 
 const light = new THREE.DirectionalLight(NOON, 1);
 light.castShadow = true;
-light.position.set(0, 1, 1);
+light.position.set(1, 1, 1);
 light.target = group;
 
 // setup shadow properties of the light
@@ -93,33 +97,9 @@ spotLight.shadow.camera.near = 0.5;
 spotLight.shadow.camera.far = 50;
 spotLight.shadow.camera.fov = 30; // Field of view for shadow camera
 
-scene.add(spotLight);
-
-const sideMaterial = new THREE.MeshStandardMaterial({
-  map: soilTexture,
-  normalMap: soilTextureNormal,
-});
-const topMaterial = new THREE.MeshStandardMaterial({
-  map: grassTop,
-  color: new THREE.Color(0x7cbd6b),
-  normalMap: grassTopNormal,
-});
-
-const materialMap = [
-  sideMaterial,
-  sideMaterial,
-  topMaterial,
-  sideMaterial,
-  sideMaterial,
-  sideMaterial,
-];
+// scene.add(spotLight);
 
 const geometries: any = [];
-
-const CHUNK_SIZE = 100;
-const MAX_HEIGHT = 10;
-
-const materials: any = [];
 
 function setUVs(geometry: any) {
   const uvs = geometry.attributes.uv.array;
@@ -163,30 +143,17 @@ function setUVs(geometry: any) {
     }
   }
   geometry.attributes.uv.needsUpdate = true;
-  // const uvs = geometry.attributes.uv.array;
-  // const uvCount = uvs.length / 2;
-
-  // // Map UVs for each face to your texture.
-  // for (let i = 0; i < uvCount; i++) {
-  //   if (i < 4) {
-  //     // Top face
-  //     uvs[i * 2] = i % 2; // X
-  //     uvs[i * 2 + 1] = Math.floor(i / 2); // Y
-  //   } else {
-  //     // Side faces
-  //     uvs[i * 2] = (i % 2) * 1; // X
-  //     uvs[i * 2 + 1] = Math.floor(i / 2) * 1; // Y
-  //   }
-  // }
-  // geometry.attributes.uv.needsUpdate = true;
 }
+
+const CHUNK_SIZE = 200;
+const MAX_HEIGHT = 15;
 
 for (let x = 0; x < CHUNK_SIZE; ++x) {
   for (let z = 0; z < CHUNK_SIZE; ++z) {
     // const height = Math.floor(Math.random() * MAX_HEIGHT);
 
     const h =
-      n.perlin2(z * (10 / CHUNK_SIZE), x * (10 / CHUNK_SIZE)) * MAX_HEIGHT;
+      n.perlin2(z * (7 / CHUNK_SIZE), x * (7 / CHUNK_SIZE)) * MAX_HEIGHT;
 
     // place blocks vertically
     for (let y = 0; y < Math.max(1, h); ++y) {
@@ -197,31 +164,18 @@ for (let x = 0; x < CHUNK_SIZE; ++x) {
 
       geometries.push(blockGeometry);
 
-      materials.push(...materialMap);
-
       setUVs(blockGeometry);
-
-      // const block = new THREE.Mesh(geometry, materialMap);
-
-      // const blockSize = 1;
-
-      // block.position.set(x * blockSize, y * blockSize, z * blockSize);
-      // block.castShadow = true;
-      // block.receiveShadow = false;
-
-      // group.add(block);
-
-      // block.frustumCulled = true;
     }
   }
 }
 
-const dirLight1 = new THREE.DirectionalLight(0xffffff, 3);
-dirLight1.position.set(1, 1, 1);
+const dirLight1 = new THREE.DirectionalLight(0xe3e3e3, 1);
+dirLight1.position.set(0, 1, 1);
+scene.add(dirLight1);
 
-const pointLight = new THREE.PointLight(0xffffff, 1, 100);
-pointLight.position.set(-10, 10, 10);
-//scene.add(pointLight);
+// const dirLight2 = new THREE.DirectionalLight(NOON, 1);
+// dirLight1.position.set(-1, -1, -1);
+// scene.add(dirLight2);
 
 group.frustumCulled = true;
 
@@ -232,47 +186,56 @@ const mesh = new THREE.Mesh(
   new THREE.MeshStandardMaterial({ map: grassAtlas }),
 );
 
-//log(merged.groups)
-// log(materials);
-
-//log(geometry.groups)
-
-// log(merged.attributes.uv);
-
 mesh.castShadow = true;
 mesh.receiveShadow = false;
 
-// scene.add(mesh);
 group.add(mesh);
 
 function animate() {
+  stats.begin();
   controls.update();
 
-  //camera.position.x += 0.3
+  stats.end();
 
   renderer.render(scene, camera);
 }
 
-//const hemisphere_light = new THREE.HemisphereLight(0xffffbb, 0x080820, 1);
-const hemisphere_light = new THREE.HemisphereLight(0xfb9062, 0x080820, 1);
+//const hemisphere_light = new THREE.HemisphereLight(0xfb9062, 0x080820, 1);
 
 scene.add(group);
-scene.add(light);
-scene.add(hemisphere_light);
+// scene.add(hemisphere_light);
 
-const ambientLight = new THREE.AmbientLight(0x555555, 4);
-scene.add(ambientLight);
+// const ambientLight = new THREE.AmbientLight(0x555555, 1);
+// scene.add(ambientLight);
 
 const controls = new MapControls(camera, renderer.domElement);
 
 controls.enableDamping = true;
 controls.dampingFactor = 0.05;
 
+controls.autoRotate = true;
+controls.autoRotateSpeed = 0.08;
+
 controls.screenSpacePanning = false;
 
 controls.minDistance = 1;
 controls.maxDistance = 500;
 
-document.body.appendChild(renderer.domElement);
+const app = document.getElementById("app");
+
+app?.appendChild(renderer.domElement);
+
+const button = document.createElement("button");
+button.innerHTML = "anti-aliasing ON";
+button.style.position = "absolute";
+button.style.top = "0";
+button.style.left = "0";
+button.style.margin = "6px";
+
+// app?.appendChild(button);
 
 renderer.setAnimationLoop(animate);
+
+const stats = new Stats();
+stats.showPanel(1);
+app?.appendChild(stats.dom);
